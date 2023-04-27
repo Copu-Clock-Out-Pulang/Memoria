0306628f80fdd3213dccd2d3db1efaaedf4fa636
Winxen <winxeneric@gmail.com>
2023-04-27 16:28:33 +0700

    feat: Destination migration (#12)
    Co-authored-by: Adryan Eka Vandra <adryanekavandra@gmail.com>

---
+++ Memoria/Features/Destination/Presentation/ViewModel/DestinationViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  DestinationViewModel.swift
+//  Memoria
+//
+//  Created by Winxen Ryandiharvin on 25/04/23.
+//
+
+import Foundation
+import CoreData
+import SwiftUI
+
+class DestinationViewModel: ObservableObject {
+
+}
