5f494900bfe4a81ff720fd1c8aa5c4c8e0d99b11
winxene <winxeneric@gmail.com>
2023-04-28 17:27:09 +0700

    feat: added DeleteScrapPage usecase

--- Memoria/Features/Scrapbook/Domain/Usecases/DeleteScrapPage.swift
+++ Memoria/Features/Scrapbook/Domain/Usecases/DeleteScrapPage.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import Combine
+
+protocol DeleteScrapPage: UseCaseProtocol {
+    associatedtype ReturnType = Void
+    associatedtype Params = DeleteScrapPageParams
+}
+
+class DeleteScrapPageImpl: DeleteScrapPage {
+
+    let repository: ScrapPageRepository
+
+    init(repository: ScrapPageRepository) {
+        self.repository = repository
+    }
+
+    func execute(params: DeleteScrapPageParams) -> AnyPublisher<(), Failure> {
+        repository.deleteScrapPage(scrapPage: params.scrapPage)
+    }
+}
+
+struct DeleteScrapPageParams: Equatable {
+    let scrapPage: ScrapPage
+}
