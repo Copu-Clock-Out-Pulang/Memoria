b918a543a505594656489fd2e46e43856b414ce4
winxene <winxeneric@gmail.com>
2023-04-28 17:22:32 +0700

    feat: added EditScrapPage usecase

--- Memoria/Features/Scrapbook/Domain/Usecases/EditScrapPage.swift
+++ Memoria/Features/Scrapbook/Domain/Usecases/EditScrapPage.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import Combine
+
+protocol EditScrapPage: UseCaseProtocol {
+    associatedtype ReturnType = ScrapPage
+    associatedtype Params = EditScrapPageParams
+}
+
+class EditScrapPageImpl: EditScrapPage {
+
+    let repository: ScrapPageRepository
+
+    init(repository: ScrapPageRepository) {
+        self.repository = repository
+    }
+
+    func execute(params: EditScrapPageParams) -> AnyPublisher<ScrapPage, Failure> {
+        let editedScrapPage = params.scrapPage.with(
+            name: params.name,
+            thumbnail: params.thumbnail,
+            content: params.content,
+            updatedAt: Date()
+        )
+        return repository.editScrapPage(scrapPage: editedScrapPage)
+    }
+}
+
+struct EditScrapPageParams: Equatable {
+    let scrapPage: ScrapPage
+    let name: String
+    let thumbnail: String
+    let content: String
+}
