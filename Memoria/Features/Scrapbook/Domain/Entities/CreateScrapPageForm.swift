d80829de92bc6fef6214ba4b09ba03cae7c789b6
winxene <winxeneric@gmail.com>
2023-05-02 18:14:26 +0700

    feat: added entity for CreateScrapPageForm

--- Memoria/Features/Scrapbook/Domain/Entities/CreateScrapPageForm.swift
+++ Memoria/Features/Scrapbook/Domain/Entities/CreateScrapPageForm.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct CreateScrapPageForm: Equatable {
+    let id: UUID
+    let name: String
+    let thumbnail: String
+    let content: String
+    let createdAt: Date
+    let updatedAt: Date
+
+    let scrapBook: ScrapBook
+}
