aaeb064c9da1ece6e21f74ca60d7e6513ef2d0bf
Adryan Eka Vandra <adryanekavandra@gmail.com>
2023-05-02 12:06:55 +0700

    feat: destination data layer (#16)

--- Memoria/Core/DI/InjectionContainer.swift
+++ Memoria/Core/DI/InjectionContainer.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreData
 import Swinject
 import SwinjectAutoregistration
 
@@ -31,8 +32,60 @@ final class InjectionContainer {
     private func buildContainer() -> Container {
         let container = Container()
         // register component here
+        self.registerSharedContainer(container)
+        self.registerDestinationContainer(container)
 
         return container
     }
+    
+    private func registerSharedContainer(_ container: Container) {
+        container.register(PersistenceController.self) { _ in
+            let controller = PersistenceController.shared
+            return controller
+            
+        }
+        .inObjectScope(.container)
+        
+        
+        container.register(NSManagedObjectContext.self) { resolver in
+            let controller = resolver.resolve(PersistenceController.self)!
+            let context = controller.container.viewContext
+            return context
+            
+        }
+        .inObjectScope(.container)
+    }
 
+    private func registerDestinationContainer(_ container: Container) {
+        container.autoregister(DestinationLocalDataSource.self, initializer: DestinationLocalDataSourceImpl.init)
+        container.autoregister(DestinationRepository.self, initializer: DestinationRepositoryImpl.init)
+        
+        container.autoregister(ImageSegmentor.self, initializer: ImageSegmentorImpl.init)
+        
+        container.register(DestinationImageGenerator.self) { resolver in
+            let segmentor = resolver.resolve(ImageSegmentor.self)!
+            return DestinationImageGeneratorImpl(imageSegmentor: segmentor)
+            
+        }
+        container.register(AnyUseCase<[Area], NoParams>.self, name: "GetTripArea") { resolver in
+            let repo = resolver.resolve(DestinationRepository.self)!
+            let usecase = GetTripAreaImpl(repository: repo)
+            return usecase.eraseToAnyUseCase()
+        }
+        container.register(
+            AnyUseCase<[Destination], GetTripDestinationByAreaParams>.self,
+            name: "GetTripDestinationByArea") { resolver in
+            let repo = resolver.resolve(DestinationRepository.self)!
+            let usecase = GetTripDestinationByAreaImpl(repository: repo)
+            return usecase.eraseToAnyUseCase()
+        }
+        
+        container.register(
+            AnyUseCase<[Recommendation], GenerateRecommendationParams>.self,
+            name: "GenerateRecommendation") { resolver in
+            let generator = resolver.resolve(DestinationImageGenerator.self)!
+            let usecase = GenerateRecommendationImpl(generator: generator)
+            return usecase.eraseToAnyUseCase()
+        }
+    }
 }
---
+++ Memoria/Features/Destination/Data/Datasources/DestinationLocalDataSource.swift
@@ -0,0 +1,55 @@
+//
+//  DestinationLocalDataSource.swift
+//  Memoria
+//
+//  Created by Adryan Eka Vandra on 29/04/23.
+//
+
+import Foundation
+import Combine
+import CoreData
+
+protocol DestinationLocalDataSource {
+    func fetchArea() -> AnyPublisher<[AreaCoreDataModel], Failure>
+    func fetchDestinationByArea(id: UUID) -> AnyPublisher<[DestinationCoreDataModel], Failure>
+}
+
+class DestinationLocalDataSourceImpl: DestinationLocalDataSource {
+    
+    let context: NSManagedObjectContext
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+    }
+    func fetchArea() -> AnyPublisher<[AreaCoreDataModel], Failure> {
+        let fetchReqest = AreaCoreDataModel.fetchRequest()
+        do {
+            let areas = try context.fetch(fetchReqest)
+            return Just(areas).setFailureType(to: Failure.self).eraseToAnyPublisher()
+        } catch _ {
+            return Fail(error: Failure.fetchAreaFailure).eraseToAnyPublisher()
+        }
+    }
+    
+    func fetchDestinationByArea(id: UUID) -> AnyPublisher<[DestinationCoreDataModel], Failure> {
+        let fetchRequest = AreaCoreDataModel.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "%K == %@", "id", id as CVarArg)
+        fetchRequest.fetchLimit = 1
+        
+        do {
+            guard let areas = try context.fetch(fetchRequest).first else {
+                return Fail(error: Failure.fetchDestinationFailure).eraseToAnyPublisher()
+
+            }
+            
+            return Just(areas.destinations!.toArray())
+                .setFailureType(to: Failure.self)
+                .eraseToAnyPublisher()
+            
+        } catch _ {
+            return Fail(error: Failure.fetchDestinationFailure).eraseToAnyPublisher()
+        }
+    }
+    
+    
+}
