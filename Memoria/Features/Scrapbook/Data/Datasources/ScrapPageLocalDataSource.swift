e2b9b6f3a92ae56e24ad76d86236c88bc5b06935
winxene <winxeneric@gmail.com>
2023-05-02 18:19:08 +0700

    feat: added ScrapPageLocalDataSource

--- Memoria/Features/Scrapbook/Data/Datasources/ScrapPageLocalDataSource.swift
+++ Memoria/Features/Scrapbook/Data/Datasources/ScrapPageLocalDataSource.swift
@@ -10,24 +10,76 @@ import Combine
 import CoreData
 
 protocol ScrapPageLocalDataSource {
-    func getScrapPages() -> AnyPublisher<[ScrapPageCoreDataModel], Error>
+
+    // ini smua coredata model atau ScrapPage biasa?
+
+    func createScrapPage(scrapBook: ScrapBookCoreDataModel, page: ScrapPageCoreDataModel) -> AnyPublisher<ScrapPageCoreDataModel, Failure>
+
+    func getScrapPages() -> AnyPublisher<[ScrapPageCoreDataModel], Failure>
+
+    func editScrapPage(scrapPage: ScrapPageCoreDataModel) -> AnyPublisher<ScrapPageCoreDataModel, Failure>
+
+    // yg delete itu scrappage atau scrappagedatamodel?
+    func deleteScrapPage(scrapPage: ScrapPageCoreDataModel) -> AnyPublisher<Void, Failure>
 }
 
-class ScrapPageSourceImpl: ScrapPageLocalDataSource {
-    let persistentController: PersistenceController
-    
-    init(persistentController: PersistenceController) {
-        self.persistentController = persistentController
+class ScrapPageLocalDataSourceImpl: ScrapPageLocalDataSource {
+
+    let context: NSManagedObjectContext
+
+    init(context: NSManagedObjectContext) {
+        self.context = context
     }
-    
-    func getScrapPages() -> AnyPublisher<[ScrapPageCoreDataModel], Error> {
+
+    func getScrapPages() -> AnyPublisher<[ScrapPageCoreDataModel], Failure> {
         let fetchRequest: NSFetchRequest<ScrapPageCoreDataModel> = ScrapPageCoreDataModel.fetchRequest()
         do {
-            let context = persistentController.container.viewContext
             let result = try context.fetch(fetchRequest)
-            return Just(result).setFailureType(to: Error.self).eraseToAnyPublisher()
-        } catch let error {
-            return Fail(error: error).eraseToAnyPublisher()
+            return Just(result).setFailureType(to: Failure.self).eraseToAnyPublisher()
+        } catch _ {
+            return Fail(error: Failure.getScrapPageFailure).eraseToAnyPublisher()
+        }
+    }
+
+    func createScrapPage(scrapBook: ScrapBookCoreDataModel, page: ScrapPageCoreDataModel) -> AnyPublisher<ScrapPageCoreDataModel, Failure> {
+
+        scrapBook.addToScrapPages(page)
+        do {
+            try context.save()
+            return Just(page).setFailureType(to: Failure.self).eraseToAnyPublisher()
+        } catch _ {
+            return Fail(error: Failure.createScrapPageFailure).eraseToAnyPublisher()
+        }
+    }
+
+    func editScrapPage(scrapPage: ScrapPageCoreDataModel) -> AnyPublisher<ScrapPageCoreDataModel, Failure> {
+        do {
+            let fetchedPage = try context.existingObject(with: scrapPage.objectID) as! ScrapPageCoreDataModel
+
+            fetchedPage.name = scrapPage.name
+            fetchedPage.thumbnail = scrapPage.thumbnail
+            fetchedPage.content = scrapPage.content
+            fetchedPage.updatedAt = Date.now
+
+            try context.save()
+            return Just(fetchedPage).setFailureType(to: Failure.self).eraseToAnyPublisher()
+        } catch _ {
+            return Fail(error: Failure.editScrapPageFailure).eraseToAnyPublisher()
+        }
+    }
+
+    func deleteScrapPage(scrapPage: ScrapPageCoreDataModel) -> AnyPublisher<Void, Failure> {
+        let fetchRequest = ScrapPageCoreDataModel.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "%K == %@", "id", scrapPage.id! as any CVarArg)
+        fetchRequest.fetchLimit = 1
+        do {
+            guard let scrapPage = try context.fetch(fetchRequest).first else {
+                return Fail(error: Failure.deleteScrapPageFailure).eraseToAnyPublisher()
+            }
+            context.delete(scrapPage)
+            return Just(()).setFailureType(to: Failure.self).eraseToAnyPublisher()
+        } catch _ {
+            return Fail(error: Failure.deleteScrapPageFailure).eraseToAnyPublisher()
         }
     }
 }
