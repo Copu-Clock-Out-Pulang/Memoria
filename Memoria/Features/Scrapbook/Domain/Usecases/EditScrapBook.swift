d642985d013e9a9ef358efd56b30e5d919dd31bc
winxene <winxeneric@gmail.com>
2023-05-02 18:22:25 +0700

    feat: added EditScrapBook usecase

--- Memoria/Features/Scrapbook/Domain/Usecases/EditScrapBook.swift
+++ Memoria/Features/Scrapbook/Domain/Usecases/EditScrapBook.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import Combine
+
+protocol EditScrapBook: UseCaseProtocol {
+    associatedtype ReturnType = ScrapBook
+    associatedtype Params = EditScrapBookParams
+}
+
+class EditScrapBookImpl: EditScrapBook {
+
+    let repository: ScrapBookRepository
+
+    init(repository: ScrapBookRepository) {
+        self.repository = repository
+    }
+
+    func execute(params: EditScrapBookParams) -> AnyPublisher<ScrapBook, Failure> {
+        return repository.editScrapBook(scrapBook: params.scrapBook, form: params.form)
+    }
+}
+
+struct EditScrapBookParams: Equatable {
+    let scrapBook: ScrapBook
+    let form: EditScrapBookForm
+}
