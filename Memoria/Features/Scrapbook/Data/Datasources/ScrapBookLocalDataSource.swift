c2b9579c58c9989336db1d6f555ebb350bda8abb
winxene <winxeneric@gmail.com>
2023-05-02 18:18:50 +0700

    feat: added ScrapBookLocalDataSource

--- Memoria/Features/Scrapbook/Data/Datasources/ScrapBookLocalDataSource.swift
+++ Memoria/Features/Scrapbook/Data/Datasources/ScrapBookLocalDataSource.swift
@@ -1,5 +1,5 @@
 //
-//  ScrapBookDataSource.swift
+//  ScrapBookLocalDataSource.swift
 //  Memoria
 //
 //  Created by Winxen Ryandiharvin on 29/04/23.
@@ -7,13 +7,69 @@
 
 import Foundation
 import Combine
+import CoreData
 
-protocol ScrapBookDataSource {
-    func getScrapBook() -> AnyPublisher<[ScrapBookCoreDataModel], Failure>
+protocol ScrapBookLocalDataSource {
+
+    // func createScrapBook, bingung createscrapbook paramsnya apa
+    func createScrapBook(scrapBook: ScrapBookCoreDataModel) -> AnyPublisher<ScrapBookCoreDataModel, Failure>
+
+    func getScrapBooks() -> AnyPublisher<[ScrapBookCoreDataModel], Failure>
+
+    func editScrapBook(scrapBook: ScrapBookCoreDataModel) -> AnyPublisher<ScrapBookCoreDataModel, Failure>
+
+    func deleteScrapBook(scrapBook: ScrapBookCoreDataModel) -> AnyPublisher<Void, Failure>
+
 }
 
-class ScrapBookDataSourceImpl: ScrapBookDataSource {
-    
-    func getScrapBook() -> AnyPublisher<[ScrapBookCoreDataModel], Failure> {
+class ScrapBookLocalDataSourceImpl: ScrapBookLocalDataSource {
+
+    let context: NSManagedObjectContext
+
+    init(context: NSManagedObjectContext) {
+        self.context = context
+    }
+
+    func createScrapBook(scrapBook: ScrapBookCoreDataModel) -> AnyPublisher<ScrapBookCoreDataModel, Failure> {
+        do {
+            try context.save()
+            return Just(scrapBook).setFailureType(to: Failure.self).eraseToAnyPublisher()
+        } catch _ {
+            return Fail(error: Failure.createScrapBookFailure).eraseToAnyPublisher()
+        }
+    }
+
+    func editScrapBook(scrapBook: ScrapBookCoreDataModel) -> AnyPublisher<ScrapBookCoreDataModel, Failure> {
+        do {
+            let fetchedScrapBook = try context.existingObject(with: scrapBook.objectID) as! ScrapBookCoreDataModel
+
+            fetchedScrapBook.name = scrapBook.name
+
+
+            try context.save()
+            return Just(fetchedScrapBook).setFailureType(to: Failure.self).eraseToAnyPublisher()
+        } catch _ {
+            return Fail(error: Failure.editScrapBookFailure).eraseToAnyPublisher()
+        }
+    }
+
+    func deleteScrapBook(scrapBook: ScrapBookCoreDataModel) -> AnyPublisher<Void, Failure> {
+        do {
+            context.delete(scrapBook)
+            try context.save()
+            return Just(()).setFailureType(to: Failure.self).eraseToAnyPublisher()
+        } catch _ {
+            return Fail(error: Failure.deleteScrapBookFailure).eraseToAnyPublisher()
+        }
+    }
+
+    func getScrapBooks() -> AnyPublisher<[ScrapBookCoreDataModel], Failure> {
+        let fetchRequest: NSFetchRequest<ScrapBookCoreDataModel> = ScrapBookCoreDataModel.fetchRequest()
+        do {
+            let result = try context.fetch(fetchRequest)
+            return Just(result).setFailureType(to: Failure.self).eraseToAnyPublisher()
+        } catch _ {
+            return Fail(error: Failure.getScrapBookFailure).eraseToAnyPublisher()
+        }
     }
 }
