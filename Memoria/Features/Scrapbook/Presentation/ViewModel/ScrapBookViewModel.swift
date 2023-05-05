fce298787ea444b15cb3b8b5e1ab7a138553467c
winxene <winxeneric@gmail.com>
2023-05-05 15:30:12 +0700

    feat: added template file for scrapbook view model

--- Memoria/Features/Scrapbook/Presentation/ViewModel/ScrapBookViewModel.swift
+++ Memoria/Features/Scrapbook/Presentation/ViewModel/ScrapBookViewModel.swift
@@ -1,20 +1,46 @@
+////
+////  ScrapPageViewModel.swift
+////  Memoria
+////
+////  Created by Winxen Ryandiharvin on 03/05/23.
+////
 //
-//  SwiftUIView.swift
-//  Memoria
+// import Foundation
+// import Combine
 //
-//  Created by Winxen Ryandiharvin on 03/05/23.
+// class ScrapPageViewModel: ObservableObject {
 //
-
-import SwiftUI
-
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwiftUIView()
-    }
-}
+//    private let createScrapPage: AnyUseCase<ScrapPage, Failure>
+//    private let getScrapPages: AnyUseCase<[ScrapPage], Failure>
+//    private let editScrapPage: AnyUseCase<ScrapPage, Failure>
+//    private let deleteScrapPage: AnyUseCase<ScrapPage, Failure>
+//    private var cancellables: Set<AnyCancellable>
+//
+//    //Q: perlu panggil si initial ka?
+////    @Published private(set) var state = ScrapPage.initial()
+//
+//    init(createScrapPage: AnyUseCase<ScrapPage, Failure>, getScrapPages: AnyUseCase<[ScrapPage], Failure>, editScrapPage: AnyUseCase<ScrapPage, Failure>, deleteScrapPage: AnyUseCase<ScrapPage, Failure>, cancellables: Set<AnyCancellable>) {
+//        self.createScrapPage = createScrapPage
+//        self.getScrapPages = getScrapPages
+//        self.editScrapPage = editScrapPage
+//        self.deleteScrapPage = deleteScrapPage
+//        self.cancellables = cancellables
+//    }
+//    func loadProcess(intent: ScrapPageIntent) {
+//        switch intent {
+//        case .getScrapPages: getScrapPages()
+//        }
+//    }
+// }
+//
+// enum ScrapPageStatus {
+//    case initial
+//    case loading
+//    case successGetScrapPage([ScrapPage])
+//    case successScrapPage(ScrapPage)
+//    case failed(Failure)
+// }
+//
+// enum ScrapPageIntent {
+//    case getScrapPages
+// }
