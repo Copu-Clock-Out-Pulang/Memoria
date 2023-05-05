6c42c03678ba48c9d68382fdddb8cd4de3e76f22
winxene <winxeneric@gmail.com>
2023-05-06 02:35:28 +0700

    feat: added ScrapBookDetailViewController

--- Memoria/Features/Scrapbook/Presentation/Pages/ScrapBookDetailViewController.swift
+++ Memoria/Features/Scrapbook/Presentation/Pages/ScrapBookDetailViewController.swift
@@ -8,19 +8,58 @@
 import UIKit
 import SwiftUI
 import SnapKit
+import Combine
 
-class ScrapBookDetailViewController: UIViewController {
+class ScrapBookDetailViewController: UIViewController, ObservableObject {
 
     private let scrapBookTitle = "Page Title"
+    private var cancellables = Set<AnyCancellable>()
+    private let scrapPageViewModel = InjectionContainer.shared.container.resolve(ScrapPageViewModel.self)!
+    private let scrapBookViewModel = InjectionContainer.shared.container.resolve(ScrapBookViewModel.self)!
+
+    //    let getScrapBook = InjectionContainer.shared.container.resolve(AnyUseCase<[ScrapBook], NoParams>.self, name: "GetScrapBook")
+
+    func navigateToScrapPageEditor() {
+        navigationController?.pushViewController(ScrapbookEditorViewController(scrapPageName: "", scrapPageContent: "", scrapPageThumbnail: "", viewModel: scrapPageViewModel), animated: true)
+    }
+
+    @Published var scrapBook: ScrapBook?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        //        createScrapBook?.execute(params: CreateScrapBookParams(form: CreateScrapBookForm(user: User(id: UUID(), name: "Duar"), name: "ScrapBookDummy", quote: "Jalan jalan ke kota baru", scrapPages: [ScrapPage(id: UUID(), name: "Duar", thumbnail: "", content: "", createdAt: Date.now, updatedAt: Date.now)], selectedRecommendations: [], startDate: Date.now, endDate: Date.now)))
+        //            .sink(receiveCompletion: {completion in
+        //                switch completion {
+        //                case .finished:
+        //                    break
+        //                case .failure(let failure):
+        //                    break
+        //                }
+        //            }, receiveValue: {[weak self] scrapbook in
+        //                self?.scrapBook = scrapbook
+        //            }).store(in: &cancellables)
+
+        //        getScrapBook?.execute(params: NoParams())
+        //            .sink(receiveCompletion: {completion in
+        //            switch completion {
+        //            case .finished:
+        //                break
+        //            case .failure(let failure):
+        //                break
+        //            }
+        //        }, receiveValue: {[weak self] scrapbook in
+        //            self?.scrapBook = scrapbook.first
+        //        }).store(in: &cancellables)
+
+        scrapBookViewModel.loadScrapBooks()
+        scrapPageViewModel.loadScrapPages()
 
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Poppins-Bold", size: 22)!]
 
         self.title = scrapBookTitle
 
-        let rootView = ScrapBookDetailView()
+        let rootView = ScrapBookDetailView(controller: self, scrapBookViewModel: scrapBookViewModel, scrapPageViewModel: scrapPageViewModel)
 
         let hostingController = UIHostingController(rootView: rootView)
 
