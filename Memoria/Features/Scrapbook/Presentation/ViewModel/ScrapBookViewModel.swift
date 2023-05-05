66aa18e0c48ce376b17d9fd640c2b266e3f35c86
winxene <winxeneric@gmail.com>
2023-05-06 02:27:40 +0700

    feat: added scrapbook and scrappage view model

--- Memoria/Features/Scrapbook/Presentation/ViewModel/ScrapBookViewModel.swift
+++ Memoria/Features/Scrapbook/Presentation/ViewModel/ScrapBookViewModel.swift
@@ -6,3 +6,99 @@
 //
 
 import Foundation
+import Combine
+
+class ScrapBookViewModel: ObservableObject {
+
+    // MARK: - Attributes
+    @Published private(set) var scrapBook: ScrapBook?
+    @Published private(set) var status: ScrapBookStatus = .initial
+
+    // MARK: - Usecases
+    private let createScrapBook: AnyUseCase<ScrapBook, CreateScrapBookParams>
+    private let getScrapBooks: AnyUseCase<[ScrapBook], NoParams>
+    private let editScrapBook: AnyUseCase<ScrapBook, EditScrapBookParams>
+    private let deleteScrapBook: AnyUseCase<Void, DeleteScrapBookParams>
+
+    // MARK: - Cancellables
+    private var cancellables = Set<AnyCancellable>()
+
+    init(createScrapBook: AnyUseCase<ScrapBook, CreateScrapBookParams>, getScrapBooks: AnyUseCase<[ScrapBook], NoParams>, editScrapBook: AnyUseCase<ScrapBook, EditScrapBookParams>, deleteScrapBook: AnyUseCase<Void, DeleteScrapBookParams>) {
+        self.createScrapBook = createScrapBook
+        self.getScrapBooks = getScrapBooks
+        self.editScrapBook = editScrapBook
+        self.deleteScrapBook = deleteScrapBook
+    }
+
+    func loadScrapBooks() {
+        status = .loading
+        getScrapBooks.execute(params: NoParams())
+            .sink(receiveCompletion: {completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let failure):
+                    break
+                }
+            }, receiveValue: {scrapbook in
+                self.scrapBook = scrapbook.first
+            }).store(in: &cancellables)
+    }
+
+    func addScrapBook(form: CreateScrapBookForm) {
+        createScrapBook.execute(params: CreateScrapBookParams(form: form))
+            .sink(receiveCompletion: {completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let failure):
+                    break
+                }
+            }, receiveValue: {scrapbook in
+                self.scrapBook = scrapbook
+                self.loadScrapBooks()
+            }).store(in: &cancellables)
+    }
+
+    func updateScrapBook(scrapBook: ScrapBook, form: EditScrapBookForm) {
+        editScrapBook.execute(params: EditScrapBookParams(scrapBook: scrapBook, form: form))
+            .sink(receiveCompletion: {completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let failure):
+                    break
+                }
+            }, receiveValue: {scrapbook in
+                self.scrapBook = scrapbook
+                self.loadScrapBooks()
+            }).store(in: &cancellables)
+    }
+
+    func removeScrapBook(scrapBook: ScrapBook) {
+        deleteScrapBook.execute(params: DeleteScrapBookParams(scrapBook: scrapBook))
+            .sink(receiveCompletion: {completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let failure):
+                    break
+                }
+            }, receiveValue: {
+                self.loadScrapBooks()
+            }).store(in: &cancellables)
+    }
+}
+
+enum ScrapBookStatus {
+    case initial
+    case loading
+    case successGetScrapBook([ScrapBook])
+    case successCreateScrapBook(ScrapBook)
+    case successDeleteScrapBook
+    case failed(Failure)
+}
+
+enum ScrapBookIntent {
+    case getScrapBooks
+}
--- Memoria/Features/Scrapbook/Presentation/ViewModel/ScrapPageViewModel.swift
+++ Memoria/Features/Scrapbook/Presentation/ViewModel/ScrapPageViewModel.swift
@@ -5,63 +5,100 @@
 //  Created by Winxen Ryandiharvin on 03/05/23.
 //
 
- import Foundation
- import Combine
+import Foundation
+import Combine
 
- class ScrapPageViewModel {
-
+class ScrapPageViewModel: ObservableObject {
     // MARK: - Attributes
-     @Published private(set) var scrapPageId: UUID
-     @Published private(set) var scrapPageName: String?
-     @Published private(set) var scrapPageThumbnail: String?
-     @Published private(set) var scrapPageContent: String?
-     @Published private(set) var scrapBook: ScrapBook?
-     @Published private(set) var createdAt: Date
-     @Published private(set) var updatedAt: Date
-     @Published private(set) var status: ScrapPageStatus = .initial
-     
+    @Published private(set) var scrapPage: ScrapPage?
+    @Published private(set) var status: ScrapPageStatus = .initial
+
     // MARK: - Usecases
     private let createScrapPage: AnyUseCase<ScrapPage, CreateScrapPageParams>
-    private let getScrapPages: AnyUseCase<[ScrapPage], Failure>
-    private let editScrapPage: AnyUseCase<ScrapPage, Failure>
-    private let deleteScrapPage: AnyUseCase<ScrapPage, Failure>
-     
-    // MARK: - Cancellables
-    private var cancellables: Set<AnyCancellable>
+    private let getScrapPages: AnyUseCase<[ScrapPage], NoParams>
+    private let editScrapPage: AnyUseCase<ScrapPage, EditScrapPageParams>
+    private let deleteScrapPage: AnyUseCase<Void, DeleteScrapPageParams>
 
-     init(createScrapPage: AnyUseCase<ScrapPage, Failure>, getScrapPages: AnyUseCase<[ScrapPage], Failure>, editScrapPage: AnyUseCase<ScrapPage, Failure>, deleteScrapPage: AnyUseCase<ScrapPage, Failure>) {
-         self.createScrapPage = createScrapPage
-         self.getScrapPages = getScrapPages
-         self.editScrapPage = editScrapPage
-         self.deleteScrapPage = deleteScrapPage
-     }
-    
-    func loadProcess() {
+    // MARK: - Cancellables
+    private var cancellables = Set<AnyCancellable>()
+
+    init(createScrapPage: AnyUseCase<ScrapPage, CreateScrapPageParams>, getScrapPages: AnyUseCase<[ScrapPage], NoParams>, editScrapPage: AnyUseCase<ScrapPage, EditScrapPageParams>, deleteScrapPage: AnyUseCase<Void, DeleteScrapPageParams>) {
+        self.createScrapPage = createScrapPage
+        self.getScrapPages = getScrapPages
+        self.editScrapPage = editScrapPage
+        self.deleteScrapPage = deleteScrapPage
+    }
+
+    func loadScrapPages() {
         status = .loading
-        getScrapBook?.execute(params: NoParams())
+        getScrapPages.execute(params: NoParams())
             .sink(receiveCompletion: {completion in
-            switch completion {
-            case .finished:
-                break
-            case .failure(let failure):
-                break
-            }
-        }, receiveValue: {[weak self] scrapbook in
-            self?.scrapBook = scrapbook.first
-        }).store(in: &cancellables)
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let failure):
+                    break
+                }
+            }, receiveValue: {scrappage in
+                self.scrapPage = scrappage.first
+            }).store(in: &cancellables)
+    }
+
+    func saveScrapPage(name: String, thumbnail: String, content: String) {
+        editScrapPage.execute(params: EditScrapPageParams(scrapPage: scrapPage!, form: EditScrapPageForm(name: name, thumbnail: thumbnail, content: content)))
+            .sink(receiveCompletion: {completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let failure):
+                    break
+                }
+            }, receiveValue: {
+                scrappage in
+                self.scrapPage = scrappage
+                self.loadScrapPages()
+            }).store(in: &cancellables)
+    }
+    func addScrapPage(form: CreateScrapPageForm) {
+        createScrapPage.execute(params: CreateScrapPageParams(form: form))
+            .sink(receiveCompletion: {completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let failure):
+                    break
+                }
+            }, receiveValue: {
+                scrappage in
+                self.scrapPage = scrappage
+                self.loadScrapPages()
+            }).store(in: &cancellables)
+    }
+
+    func deleteScrapPage(scrapPage: ScrapPage) {
+        deleteScrapPage.execute(params: DeleteScrapPageParams(scrapPage: scrapPage))
+            .sink(receiveCompletion: {completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let failure):
+                    break
+                }
+            }, receiveValue: {
+                self.loadScrapPages()
+            }).store(in: &cancellables)
     }
 }
- 
 
- enum ScrapPageStatus {
+enum ScrapPageStatus {
     case initial
     case loading
     case successGetScrapPage([ScrapPage])
     case successCreateScrapPage(ScrapPage)
     case successDeleteScrapPage
     case failed(Failure)
- }
+}
 
- enum ScrapPageIntent {
+enum ScrapPageIntent {
     case getScrapPages
- }
+}
