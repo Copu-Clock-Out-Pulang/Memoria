77e06cf35d7df06db42001aef0a30d7b03eaf71b
winxene <winxeneric@gmail.com>
2023-05-06 02:34:56 +0700

    feat: added ScrapBookDetailView

--- Memoria/Features/Scrapbook/Presentation/Pages/ScrapBookDetailView.swift
+++ Memoria/Features/Scrapbook/Presentation/Pages/ScrapBookDetailView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ScrapBookDetailView: View {
+
+    @ObservedObject var controller: ScrapBookDetailViewController
+    @ObservedObject var scrapBookViewModel: ScrapBookViewModel
+    @ObservedObject var scrapPageViewModel: ScrapPageViewModel
+
     var body: some View {
         GeometryReader {
             _ in
@@ -16,14 +21,16 @@ struct ScrapBookDetailView: View {
                 VStack {
                     Spacer()
                     ScrapBookCard(
-                        tripName: "Trip Name",
+                        tripName: controller.scrapBook?.name ?? "Trip Name",
                         tripDate: "27 Jan - 3 Feb 2023",
                         tripDescription:
-                            "Liburan terbaik bukanlah soal kemewahan, tetapi soal kebersamaan di tengah keluarga tercinta."
+                            controller.scrapBook?.quote ??
+                            "Liburan terbaik bukanlah soal kemewahan, tetapi soal kebersamaan di tengah keluarga tercinta.", scrapPages: controller.scrapBook?.scrapPages ?? []
                         //                        "Jalan-jalan ke kota baru"
                     )
                 }
             }
-        }
+        }.environmentObject(scrapBookViewModel).environmentObject(scrapPageViewModel)
+        .environmentObject(controller)
     }
 }
