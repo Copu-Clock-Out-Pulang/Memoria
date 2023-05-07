1300489fa3e6b13082b7010865e2b45045be7efa
Ronald Sumichael <35445753+SuRonald@users.noreply.github.com>
2023-05-08 00:51:22 +0700

    feat: page indicator (#27)

--- Memoria/Features/Destination/Presentation/Components/TripNameFormUI.swift
+++ Memoria/Features/Destination/Presentation/Components/TripNameFormUI.swift
@@ -21,37 +21,41 @@ struct TripNameFormUI: View {
                 Text(S.dstCreateYourTrip)
                     .font(Font.custom("LilitaOne", size: 40))
                     .foregroundColor(I.textPrimary.swiftUIColor)
+                
                 TextField(S.dstTripInputHint, text: $name)
-
                     .frame(height: 30)
                     .padding()
                     .font(Font.custom("Poppins", size: 16))
                     .background(.white)
                     .cornerRadius(30)
                     .shadow(radius: 2, y: 3)
-
-
                     .onChange(of: name) {
                         viewModel.changeTripName(name: $0)
                     }
+                
                 Spacer()
 
-                Button {
-                    viewController.navigateToDate()
-                } label: {
-                    Text(S.dstNext)
-                        .font(Font.custom("Poppins-Bold", size: 20))
-                        .frame(maxWidth: .infinity, minHeight: 50)
+                VStack {
+                    Button {
+                        viewController.navigateToDate()
+                    } label: {
+                        Text(S.dstNext)
+                            .font(Font.custom("Poppins-Bold", size: 20))
+                            .frame(maxWidth: .infinity, minHeight: 50)
 
+                    }
+                    .disabled(name.isEmpty)
+                    .background(name.isEmpty ?
+                                    I.disabledButton.swiftUIColor :
+                                    I.primaryButton.swiftUIColor)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+                    .shadow(radius: 2, y: 3)
+                    
+                    Image("Indicators/1")
+                        .padding(.top, 25)
                 }
-                .disabled(name.isEmpty)
-                .background(name.isEmpty ?
-                                I.disabledButton.swiftUIColor :
-                                I.primaryButton.swiftUIColor)
-                .foregroundColor(.white)
-
-                .clipShape(Capsule())
-                .shadow(radius: 2, y: 3)
+                
             }
             .padding(.all, 40)
 
